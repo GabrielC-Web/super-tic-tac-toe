982e1565a70cfa89e8299d33114aa8fa89172e7f
GabrielC-Web <gabrieljose.cz21@gmail.com>
2024-06-02 22:33:35 -0400

    Creacion

---
+++ src/components/Gameboard.svelte
@@ -0,0 +1,37 @@
+<section class="relative h-full w-full max-h-[700px] max-w-[700px]">
+    <hr class="gameboard_line_horizontal horizontal1" />
+    <hr class="gameboard_line_horizontal horizontal2" />
+    <hr class="gameboard_line_vertical vertical1" />
+    <hr class="gameboard_line_vertical vertical2" />
+</section>
+
+<style>
+    .gameboard_grid {
+        /* display: grid;
+        grid-template-columns: repeat(3, fr); */
+    }
+
+    .horizontal1 {
+        top: 33.3333333%;
+    }
+
+    .horizontal2 {
+        bottom: 33.3333333%;
+    }
+
+    .vertical1 {
+        left: 33.3333333%;
+    }
+
+    .vertical2 {
+        right: 33.3333333%;
+    }
+
+    .gameboard_line_horizontal {
+        @apply w-full h-4 absolute bg-black;
+    }
+
+    .gameboard_line_vertical {
+        @apply w-4 h-full absolute bg-black;
+    }
+</style>
