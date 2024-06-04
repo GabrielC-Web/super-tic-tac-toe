c8651d425ee44449c519bdff199d21dd461359da
GabrielC-Web <gabrieljose.cz21@gmail.com>
2024-06-03 21:30:03 -0400

    avances

--- src/components/Gameboard.svelte
+++ src/components/Gameboard.svelte
@@ -1,37 +1,27 @@
-<section class="relative h-full w-full max-h-[700px] max-w-[700px]">
-    <hr class="gameboard_line_horizontal horizontal1" />
-    <hr class="gameboard_line_horizontal horizontal2" />
-    <hr class="gameboard_line_vertical vertical1" />
-    <hr class="gameboard_line_vertical vertical2" />
+<script>
+    import Grid from "../components/Grid.svelte";
+    import { onMount } from "svelte";
+
+    let mainSquares = ["", "", "", "", "", "", "", "", ""];
+
+    onMount(() => {
+        console.log("asdad");
+    });
+</script>
+
+<section
+    class="relative gameboard_grid flex h-full w-full max-h-[700px] max-w-[700px]"
+>
+    {#each mainSquares as mainSquare}
+        <div class="border-4 border-black">
+            <Grid />
+        </div>
+    {/each}
 </section>
 
 <style>
     .gameboard_grid {
-        /* display: grid;
-        grid-template-columns: repeat(3, fr); */
-    }
-
-    .horizontal1 {
-        top: 33.3333333%;
-    }
-
-    .horizontal2 {
-        bottom: 33.3333333%;
-    }
-
-    .vertical1 {
-        left: 33.3333333%;
-    }
-
-    .vertical2 {
-        right: 33.3333333%;
-    }
-
-    .gameboard_line_horizontal {
-        @apply w-full h-4 absolute bg-black;
-    }
-
-    .gameboard_line_vertical {
-        @apply w-4 h-full absolute bg-black;
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
     }
 </style>
---
+++ src/components/Grid.svelte
@@ -0,0 +1,70 @@
+<script>
+    import { onMount } from "svelte";
+
+    let squares = [
+        {
+            value: "",
+        },
+        {
+            value: "",
+        },
+        {
+            value: "",
+        },
+        {
+            value: "",
+        },
+        {
+            value: "",
+        },
+        {
+            value: "",
+        },
+        {
+            value: "",
+        },
+        {
+            value: "",
+        },
+        {
+            value: "",
+        },
+    ];
+
+    onMount(() => {
+        console.log("asdad");
+    });
+
+    function addChoice(choice) {
+        console.log(choice);
+        squares[choice].value = "x";
+    }
+
+    const handleClick = () => {
+        console.log("asd");
+    };
+</script>
+
+<section
+    class="relative gameboard_grid flex h-full w-full max-h-[700px] max-w-[700px]"
+>
+    {#each squares as square, i}
+        <button
+            class="square border-black border-2"
+            on:click={() => {
+                addChoice(i);
+            }}>{square.value}</button
+        >
+    {/each}
+</section>
+
+<style>
+    .gameboard_grid {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+    }
+
+    .square {
+        @apply flex items-center justify-center text-5xl;
+    }
+</style>
