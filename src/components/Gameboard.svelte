a4f7d99ebf7e5db352920e11f4d4fa1ea7a10d17
GabrielC-Web <gabrieljose.cz21@gmail.com>
2024-06-24 14:48:51 -0400

    Logica para cada uno de los tableros

--- src/components/Gameboard.svelte
+++ src/components/Gameboard.svelte
@@ -4,24 +4,86 @@
 
     let mainSquares = ["", "", "", "", "", "", "", "", ""];
 
-    onMount(() => {
-        console.log("asdad");
-    });
+    let enabledBoard = 0;
+
+    let playerMove = "x";
+
+    let currentGameState;
+
+    /**
+     * Setea las directrices para el próximo movimiento
+     * @param event
+     */
+    function setBoard(event) {
+        currentGameState = event.detail.gameState;
+
+        setWinnerBoard();
+
+        enabledBoard = currentGameState.position;
+
+        //* Cambia la jugada
+        // toggleNextMove();
+    }
+
+    /**
+     * Cambia la jugada para el próximo mivimiento
+     */
+    function toggleNextMove() {
+        if (playerMove == "x") {
+            playerMove = "o";
+        } else {
+            playerMove = "x";
+        }
+    }
+
+    function setWinnerBoard() {
+        if (currentGameState.win) {
+            mainSquares[enabledBoard] = currentGameState.playerMove;
+            console.log(mainSquares);
+        }
+    }
 </script>
 
 <section
     class="relative gameboard_grid flex h-full w-full max-h-[700px] max-w-[700px]"
 >
-    {#each mainSquares as mainSquare}
-        <div class="border-4 border-black">
-            <Grid />
+    {#each mainSquares as mainSquare, i}
+        <div class="border-4 border-black relative">
+            <!-- Espacio donde va la jugada ganadora -->
+            <div
+                class="absolute flex items-center justify-center w-full h-full align-middle text-center"
+            >
+                <span class="winner_move">{mainSquare}</span>
+            </div>
+
+            <Grid
+                position={i}
+                boardDisabled={mainSquare != ""}
+                {enabledBoard}
+                {playerMove}
+                on:gameState={setBoard}
+            />
         </div>
     {/each}
+
+    <!-- <div class="max-h-[225px] max-w-[225px]">
+        <Grid />
+    </div> -->
 </section>
 
 <style>
     .gameboard_grid {
         display: grid;
         grid-template-columns: repeat(3, 1fr);
+    }
+
+    .winner_move {
+        font-size: 150px;
+    }
+
+    @media (max-width: 620px) {
+        .winner_move {
+            font-size: 20vw;
+        }
     }
 </style>
--- src/components/Grid.svelte
+++ src/components/Grid.svelte
@@ -1,54 +1,153 @@
 <script>
-    import { onMount } from "svelte";
+    import { onMount, createEventDispatcher, afterUpdate } from "svelte";
 
-    let squares = ['','','','','','','','','']
+    /**
+     * Array de cuadros del tablero
+     */
+    let squares = ["", "", "", "", "", "", "", "", ""];
 
+    /**
+     * Jugada disponible a realizar en el tablero
+     */
+    export let playerMove = "x";
+
+    /**
+     * Estado del juego
+     */
+    let gameState = { win: false, position: undefined, playerMove: undefined };
+
+    /**
+     * Indica que el tablero está deshabilitado
+     */
+    export let boardDisabled = false;
+
+    /**
+     * Emite eventos
+     */
+    const dispatch = createEventDispatcher();
+
+    /**
+     * Posición o índice del tablero
+     */
+    export let position;
+
+    /**
+     * Indica si el tablero está habilitado
+     */
+    export let enabledBoard;
+
+    onMount(() => {
+        //* Deshabilito todos los tableros menos el inicial
+        if (position != 0) {
+            boardDisabled = true;
+        }
+    });
+
+    afterUpdate(() => {
+        //* Deshabilito todos los tableros menos el inicial
+        if (position == enabledBoard) {
+            boardDisabled = false;
+        }
+    });
+
+    /**
+     * Agrega la opción al tablero
+     * @param choice
+     */
     function addChoice(choice) {
+        if (boardDisabled || squares[choice] != "") {
+            return;
+        }
 
-        squares[choice] = "x";
+        //* Agrego la opción escogida al tablero
+        squares[choice] = playerMove;
 
-        console.log(hasThreeConsecutiveDiagonal(squares))
+        // gameFinished = hasMatch(squares);
+        gameState = hasMatch(squares, choice);
 
+        if (!gameState.win) {
+            boardDisabled = true;
+        }
+
+        //* Emito el evento con el update del juego
+        dispatch("gameState", {
+            gameState,
+        });
     }
 
     /**
-     * Revisa si hay 3 items que coinciden de forma diagonal
+     * Revisa si hay 3 items que coinciden en una fila o columna
      * @param arr
      */
-    function hasThreeConsecutiveDiagonal(arr) {
-        // Check main diagonal (top-left to bottom-right)
-        let mainDiagonal = true;
-        for (let i = 1; i < arr.length; i++) {
-            if (arr[i][i] !== arr[0][0]) {
-                mainDiagonal = false;
-                break;
+    function hasMatch(arr, position) {
+        for (let i = 0; i < arr.length; i++) {
+            const element = arr[i];
+
+            if (element != "") {
+                //* Para ver si hay 3 seguidos en fila 1
+                if (arr[0] != "" && arr[0] == arr[1] && arr[0] == arr[2]) {
+                    return { win: true, playerMove: arr[0] };
+                    //* Para ver si hay 3 seguidos en fila 2
+                } else if (
+                    arr[3] != "" &&
+                    arr[3] == arr[4] &&
+                    arr[3] == arr[5]
+                ) {
+                    return { win: true, playerMove: arr[3] };
+                    //* Para ver si hay 3 seguidos en fila 3
+                } else if (
+                    arr[6] != "" &&
+                    arr[6] == arr[7] &&
+                    arr[6] == arr[8]
+                ) {
+                    return { win: true, playerMove: arr[6] };
+                    //* Para ver si hay 3 seguidos en columna
+                } else if (element == arr[i + 3] && element == arr[i + 6]) {
+                    return { win: true, playerMove: element };
+                    //* Para ver si hay 3 seguidos en diagonal
+                } else if (
+                    arr[0] != "" &&
+                    arr[0] == arr[4] &&
+                    arr[0] == arr[8]
+                ) {
+                    return { win: true, playerMove: arr[0] };
+                    //* Para ver si hay 3 seguidos en diagonal inverso
+                } else if (
+                    arr[2] != "" &&
+                    arr[2] == arr[4] &&
+                    arr[2] == arr[6]
+                ) {
+                    return { win: true, playerMove: arr[2] };
+                }
             }
         }
-
-        // Check secondary diagonal (top-right to bottom-left)
-        let secondaryDiagonal = true;
-        for (let i = 1; i < arr.length; i++) {
-            if (arr[i][arr.length - 1 - i] !== arr[0][arr.length - 1]) {
-                secondaryDiagonal = false;
-                break;
-            }
-        }
-
-        return mainDiagonal || secondaryDiagonal;
+        return { win: false, position: position };
     }
-
 </script>
 
 <section
-    class="relative gameboard_grid flex h-full w-full max-h-[700px] max-w-[700px]"
+    class="relative gameboard_grid flex h-full w-full max-h-[700px] max-w-[700px] {boardDisabled
+        ? 'opacity-5'
+        : ''}"
 >
     {#each squares as square, i}
-        <button
-            class="square border-black border-2"
-            on:click={() => {
-                addChoice(i);
-            }}>{square}</button
-        >
+        <!-- En caso de que haya contenido -->
+        {#if square}
+            <button
+                class="square border-black border-2 p-0"
+                on:click={() => {
+                    addChoice(i);
+                }}>{square}</button
+            >
+            <!-- En caso de que no haya contenido -->
+        {:else}
+            <button
+                class="square border-black border-2 p-0 text-transparent"
+                on:click={() => {
+                    addChoice(i);
+                }}>{"/"}</button
+            >
+        {/if}
     {/each}
 </section>
 
@@ -60,5 +159,7 @@
 
     .square {
         @apply flex items-center justify-center text-5xl;
+        max-width: 75px;
+        max-height: 75px;
     }
 </style>
